1af5f90e01b00ceae9098330e74aa673f85f56be
Anna Ehrnsperger <hallo@annaehrnsperger.de>
2022-11-10 16:21:42 +0100

    implement intro animation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <article class="h-screen">
-    <section ref="aboutSection" class="grid-layout | p-6 pt-5 md:p-12 md:pt-9" @mouseenter="moveProjectsDown" @touchstart="moveProjectsDown">
-      <div class="grid-layout small | col-span-full pt-1 pb-12 md:p-0 md:fixed md:left-0 md:w-full md:top-10">
+    <section
+      ref="aboutSection"
+      class="grid-layout | p-6 pt-5 md:p-12 md:pt-9 h-screen flex flex-col items-center"
+      @mouseenter="moveProjectsDown"
+      @touchstart="moveProjectsDown"
+    >
+      <div ref="socials" class="grid-layout small | col-span-full pt-1 pb-12 md:p-0 md:fixed md:left-0 md:w-full md:top-10 opacity-0">
         <ul class="col-span-full md:col-start-13 md:col-span-12 flex">
           <li>
             <a href="mailto:hallo@annaehrnsperger.de" class="pr-8 group">
@@ -19,23 +24,26 @@
       </div>
 
       <div class="col-span-full lg:col-span-full">
-        <h1 class="duration-400 delay-[25ms] ease-out-quint" :class="{ 'dot-padding': activeProject === undefined }">
+        <h1 class="duration-400 delay-[25ms] ease-out-quint" :class="{ 'dot-padding': activeProject === undefined && showIntroDot }">
           <span class="relative">
-            <Dot :show="activeProject === undefined" :is-logo="true" />
+            <Dot :show="activeProject === undefined && showIntroDot" :is-logo="true" />
             <span>Anna Ehrnsperger</span>
           </span>
         </h1>
-        <h2>
-          <span class="block">Designer and Web Developer creating unique, high-quality websites.</span>
-          <span class="block pt-6 md:pt-0">I strive for beautiful execution and great user experience.</span>
-        </h2>
-        <a href="mailto:hallo@annaehrnsperger.de">Let’s create something together.</a>
+        <div>
+          <h2>
+            <span ref="introText" class="block opacity-0">Digital Design, Web Development</span>
+            <span data-about-text class="block opacity-0">Designer and Web Developer creating unique, high-quality websites.</span>
+            <span data-about-text class="block pt-6 md:pt-0 opacity-0">I strive for beautiful execution and great user experience.</span>
+          </h2>
+          <a data-about-text class="opacity-0" href="mailto:hallo@annaehrnsperger.de">Let’s create something together.</a>
+        </div>
       </div>
     </section>
 
     <section
       ref="projectsSection"
-      class="hide-scrollbar projects-section | top-projects fixed w-full overflow-y-hidden bg-black text-white"
+      class="hide-scrollbar projects-section | top-projects fixed w-full overflow-y-hidden bg-black text-white pointer-events-none opacity-0"
       @mouseenter="moveProjectsUp"
       @touchstart="moveProjectsUp"
     >
@@ -64,7 +72,7 @@
       </div>
     </section>
 
-    <section ref="previewSection" class="grid-layout | p-6 md:p-12 fixed inset-0 w-full h-full pointer-events-none">
+    <section ref="previewSection" class="grid-layout | p-6 md:p-12 fixed inset-0 w-full h-full pointer-events-none opacity-0">
       <div class="relative col-start-6 col-span-7 md:col-start-17 md:col-span-8 mt-auto aspect-square pointer-events-auto">
         <a
           v-for="(project, i) in projects"
@@ -108,6 +116,7 @@ export default {
       activeProject: undefined,
       activePreview: 0,
       initialProjectsPos: 0,
+      showIntroDot: false,
       socials: [
         { title: 'Instagram', url: 'https://www.instagram.com/annaehrnsperger/' },
         { title: 'Twitter', url: 'https://twitter.com/annaehrnsperger' },
@@ -118,11 +127,7 @@ export default {
     return seo(this.general);
   },
   mounted() {
-    this.setProjectsPos();
-
-    const { projectsSection, previewSection } = this.$refs;
-
-    gsap.set([projectsSection, previewSection], { y: this.initialProjectsPos });
+    this.introAnimation();
 
     window.addEventListener('wheel', once(this.moveProjectsUp));
     window.addEventListener('touchstart', once(this.moveProjectsUp));
@@ -131,6 +136,32 @@ export default {
     setProjectsPos() {
       const { projectsSection, aboutSection } = this.$refs;
       this.initialProjectsPos = aboutSection?.offsetHeight - projectsSection?.getBoundingClientRect().top;
+    },
+    introAnimation() {
+      const { aboutSection, introText, socials, projectsSection, previewSection } = this.$refs;
+
+      const tl = gsap.timeline({ defaults: { ease: 'power4.out', duration: 0.4 } });
+
+      tl.to(aboutSection, { delay: 0.3, onStart: () => (this.showIntroDot = true) });
+      tl.to(introText, { opacity: 1 }, '<85%');
+      tl.to(aboutSection, { height: 'auto', ease: 'expo.inOut', duration: 0.8, delay: 0.5 });
+      tl.to(introText, { opacity: 0, duration: 0.2 }, '<30%');
+      tl.set(introText, { height: 0, onComplete: () => this.setProjectsPos() });
+      tl.set([projectsSection, previewSection], { y: '120%', opacity: 1 });
+      tl.to(['[data-about-text]', socials], { opacity: 1 }, '<');
+      tl.to(
+        [projectsSection, previewSection],
+        {
+          onStart: () =>
+            gsap.to([projectsSection, previewSection], {
+              y: this.initialProjectsPos,
+              ease: 'expo.out',
+              duration: 1.6,
+              onComplete: () => gsap.set(projectsSection, { pointerEvents: 'auto' }),
+            }),
+        },
+        '<'
+      );
     },
     initScrollTrigger() {
       gsap.registerPlugin(ScrollTrigger);
